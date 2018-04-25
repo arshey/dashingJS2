ce3c0ff85db15ecad192ffd5e0f5eab526bee229
Vincent-Férié <arshey@live.fr>
2018-04-25 03:53:11 +0100

    feat(ep3): add ci part 1
    - Add travis
    - Add config for chrome (protractor & karma)

--- protractor.conf.js
+++ protractor.conf.js
@@ -1,28 +1,10 @@
-// Protractor configuration file, see link for more information
-// https://github.com/angular/protractor/blob/master/lib/config.ts
+const config = require('./protractor.conf').config;
 
-const { SpecReporter } = require('jasmine-spec-reporter');
-
-exports.config = {
-  allScriptsTimeout: 11000,
-  specs: [
-    './e2e/**/*.e2e-spec.ts'
-  ],
-  capabilities: {
-    'browserName': 'chrome'
-  },
-  directConnect: true,
-  baseUrl: 'http://localhost:4200/',
-  framework: 'jasmine',
-  jasmineNodeOpts: {
-    showColors: true,
-    defaultTimeoutInterval: 30000,
-    print: function() {}
-  },
-  onPrepare() {
-    require('ts-node').register({
-      project: 'e2e/tsconfig.e2e.json'
-    });
-    jasmine.getEnv().addReporter(new SpecReporter({ spec: { displayStacktrace: true } }));
+config.capabilities = {
+  browserName: 'chrome',
+  chromeOptions: {
+    args: ['--no-sandbox']
   }
 };
+
+exports.config = config;
